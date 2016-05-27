c081d29c53e3f13f2bd4100b7598509c979cb740
deven <deven.orie.19@dartmouth.edu>
2016-05-27 01:35:04 -0400

    All wireframe screens, Readable Code, Comments, Memmory Allocation, Cleanups

--- field_agent/src/field_agent.c
+++ field_agent/src/field_agent.c
@@ -1,43 +1,58 @@
 #include <pebble.h>
+//Deven Orie-CS50-Field Agent
+//pebble clean
+//pebble build
+//pebble install --serial /dev/cu.PebbleTime5B57-SerialPo
 
+//Windows for each screen of the app
 Window *window, *window2, *neutralize_window, *capture_window;
-MenuLayer *menu_layer, *menu_layer_two;
-TextLayer *s_text_layer, *s_text_layer_two;
-MenuLayer *keyboard;
-MenuLayer *keyboard_two;
+
+//Allows for menu built on top of scroll view
+MenuLayer *player_menu, *option_menu;
+MenuLayer *neutralize_keyboard, *capture_keyboard;
+
+//Theses are the text layers added to the navigation & capture windows
 TextLayer *navigation_header, *navigation_input;
 TextLayer *capture_header, *capture_input;
+TextLayer *neutralize_code_text, *capture_code_text;
+
 
 //Array of player names
 char *players[]={"Deven","Drew","Shirley","David","Sam"};
 
+//Array of the neutralization codes
 char *hex_test[]={"1234", "ABCD", "A1A2"};
 
+//Array of the players codes
 char *player_id[]={"1111", "AAAA", "BBBB"};
 
-
+//Symbols for the key board
 char *symbols[]={"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E", "F","G","H", "I", "J", "K", "L", "M", "N",
 "O","P","Q","R","S", "T", "U", "V", "W", "X", "Y", "Z"};
 
-
+//Player selected in screen one
 char *chosenplayer="";
 
+//Allows for concatination with the symbol chose and add it to buf
 char *chosen_symbol="";
 char *chosen_symbol_two="";
 
+//The chose symbols are added to these character arrays 
 char buf[10];
 char buf_two[10];
 
+//Prevents a code from being more than 4 characters
 int incrementer=0;
 int incrementer_two=0;
 
+//Allows for string compare to occur once 
 bool match=false;
 bool match_two=false;
 
-//First Menu
+//{The Player Menu}-Menu One
 // This is the menu item draw callback where you specify what each item should look like
 void draw_row_callback (GContext *ctx, Layer *cell_layer, MenuIndex *cell_index, void *callback_context) {
-    // Which row is it?
+
     switch (cell_index->row) {
     case 0:
         menu_cell_basic_draw(ctx, cell_layer, players[0], "Team: Lapis", NULL);
@@ -56,15 +71,15 @@ void draw_row_callback (GContext *ctx, Layer *cell_layer, MenuIndex *cell_index,
         break;
     }
 }
-
+//The Player Menu}-Menu One
 // Each section has a number of items;  we use a callback to specify this
-// You can also dynamically add and remove items using this
-uint16_t num_rows_callback (MenuLayer *menu_layer, uint16_t section_index, void *callback_context) {
+uint16_t num_rows_callback (MenuLayer *player_menu, uint16_t section_index, void *callback_context) {
   return 5;
 }
 
-//Select Button 
-void select_click_callback (MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
+//{The Player Menu}-Menu One
+//Select Button for the Player Menu
+void select_click_callback (MenuLayer *player_menu, MenuIndex *cell_index, void *callback_context) {
   int which = cell_index->row;
   chosenplayer=players[which];
   //window_stack_remove(window_stack_get_top_window());
@@ -72,9 +87,10 @@ void select_click_callback (MenuLayer *menu_layer, MenuIndex *cell_index, void *
   window_stack_push(window2,true);
 }
 
-//Second Menu
+//{Option Menu}}-Menu Two
+// This is the menu item draw callback where you specify what each item should look like
 void draw_row_callback_two (GContext *ctx, Layer *cell_layer, MenuIndex *cell_index, void *callback_context) {
-    // Which row is it?
+
     switch (cell_index->row) {
     case 0:
         menu_cell_basic_draw(ctx, cell_layer, "Neutralize Code", NULL, NULL);
@@ -85,26 +101,29 @@ void draw_row_callback_two (GContext *ctx, Layer *cell_layer, MenuIndex *cell_in
 
     }
 }
-
-uint16_t num_rows_callback_two (MenuLayer *menu_layer_two, uint16_t section_index, void *callback_context) {
+//{Option Menu}}-Menu Two
+//Sets the amount of rows for the Second Menu
+uint16_t num_rows_callback_two (MenuLayer *player_menu, uint16_t section_index, void *callback_context) {
   return 2;
 }
 
-
-
-void select_click_callback_two (MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
+//{Option Menu}}-Menu Two
+//Select for the Second Menu
+void select_click_callback_two (MenuLayer *player_menu, MenuIndex *cell_index, void *callback_context) {
   int option = cell_index->row;
   if (option==0){
+    //Go the the neutralize screen
     window_stack_push(neutralize_window,true);
   } else{
+    //Go the the capture screen
     window_stack_push(capture_window,true);
   }
 
 }
 
-//Third Menu
+//{Universal Key Board}}-Menu Three & Menu Four (Neutralize Menu & Capture Menu)
+// This is the menu item draw callback where you specify what each item should look like
 void draw_row_callback_three (GContext *ctx, Layer *cell_layer, MenuIndex *cell_index, void *callback_context) {
-    // Which row is it?
     switch (cell_index->row) {
     case 0:
         menu_cell_basic_draw(ctx, cell_layer,symbols[0] , NULL, NULL);
@@ -217,34 +236,45 @@ void draw_row_callback_three (GContext *ctx, Layer *cell_layer, MenuIndex *cell_
     }
 }
 
-// Each section has a number of items;  we use a callback to specify this
-// You can also dynamically add and remove items using this
-uint16_t num_rows_callback_three (MenuLayer *keyboard, uint16_t section_index, void *callback_context) {
+//{Universal Key Board}}-Menu Three & Menu Four (Neutralize Menu & Capture Menu)
+//Each section has a number of items;  we use a callback to specify this
+//Sets number of rows in the menu
+uint16_t num_rows_callback_three (MenuLayer *neutralize_keyboard, uint16_t section_index, void *callback_context) {
   return 36;
 }
 
-//Select Button 
-void select_click_callback_three (MenuLayer *keyboard, MenuIndex *cell_index, void *callback_context) {
+//{Key Board}}-Neutralize KeyBoard
+//Select Button to input code to neutralize
+void select_click_callback_three (MenuLayer *neutralize_keyboard, MenuIndex *cell_index, void *callback_context) {
   int which = cell_index->row;
   int index = sizeof(hex_test);
 
-
+  //if the code is not greater than for characters 
   if (incrementer!=4){
+    //symbol that is chosen from the keyboard
     chosen_symbol=symbols[which];
+    //concatinate the buffer with the chose symbol to create a code (string)
     strcat(buf, chosen_symbol);  
-
+    //add the concatinated string to the navigation output showing what character have already been entered
     text_layer_set_text(navigation_input, buf);
+    //Sleep for 1/2 second 
     psleep(500);
+    //Increment 
     incrementer++;
   }
 
+  //Once we have 4 characters
   if (incrementer==4){
+    //Iterate though the array of hex_codes
     for (int i=0; i<index; i++){
+      //Comapre the strings 
       if (strcmp(buf,hex_test[i]) == 0){
+        //Set to true if there is a match
         match=true;
       }
     }//for loop
 
+    //If a match is found print out the following else....
     if (match){
       text_layer_set_text(navigation_header, "Neutralized!");
       psleep(1000);
@@ -254,7 +284,7 @@ void select_click_callback_three (MenuLayer *keyboard, MenuIndex *cell_index, vo
       psleep(1000);
 
     } 
-
+    //Resets all of the booleans, integers, and buffers
     match=false;    
     //char buf[10];
 
@@ -270,26 +300,37 @@ void select_click_callback_three (MenuLayer *keyboard, MenuIndex *cell_index, vo
 
 }
 
-void select_click_callback_four (MenuLayer *keyboard_two, MenuIndex *cell_index, void *callback_context) {
+//{Key Board}}-Capture KeyBoard
+//Select Button to input to capture players
+void select_click_callback_four (MenuLayer *capture_keyboard, MenuIndex *cell_index, void *callback_context) {
    int which = cell_index->row;
    int index = sizeof(hex_test);
 
-
+  //if the code is not greater than for characters 
   if (incrementer_two!=4){
+    //symbol that is chosen from the keyboard
     chosen_symbol_two=symbols[which];
+    //concatinate the buffer with the chose symbol to create a code (string)
     strcat(buf_two, chosen_symbol_two);  
+    //add the concatinated string to the navigation output showing what character have already been entered
     text_layer_set_text(capture_input, buf_two);
+    //Sleep for 1/2 second 
     psleep(500);
+    //increment
     incrementer_two++;
   }
-
+  //Once we have 4 characters
   if (incrementer_two==4){
+    //Iterate though the array of player_codes
     for (int i=0; i<index; i++){
+      //Comapre the strings 
       if (strcmp(buf_two,player_id[i]) == 0){
+        //Set to true if there is a match
         match_two=true;
       }
     }//for loop
 
+    //If a match is found print out the following else....
     if (match_two){
       text_layer_set_text(capture_header, "Player Captured!");
       psleep(1000);
@@ -299,6 +340,7 @@ void select_click_callback_four (MenuLayer *keyboard_two, MenuIndex *cell_index,
       psleep(1000);
 
     } 
+    //Resets all of the booleans, integers, and buffers
 
     match_two=false;    
     //char buf[10];
@@ -320,36 +362,34 @@ void select_click_callback_four (MenuLayer *keyboard_two, MenuIndex *cell_index,
 void window_load (Window *window){
 
   //origin of the (x,y,width,height)
-  menu_layer = menu_layer_create(GRect(0, 30, 144, 152));
-  menu_layer_two = menu_layer_create(GRect(0, 30, 144, 152));
-  keyboard = menu_layer_create(GRect(0, 80, 144, 90));
-  keyboard_two = menu_layer_create(GRect(0, 80, 144, 90));
+  //creates all of the menu layers
+  player_menu = menu_layer_create(GRect(0, 30, 144, 152));
+  option_menu = menu_layer_create(GRect(0, 30, 144, 152));
+  neutralize_keyboard = menu_layer_create(GRect(0, 80, 144, 90));
+  capture_keyboard = menu_layer_create(GRect(0, 80, 144, 90));
 
   // Bind the menu layer's click config provider to the window for interactivity
-  menu_layer_set_click_config_onto_window(menu_layer, window);
-
-  menu_layer_set_click_config_onto_window(menu_layer_two, window2);
-
-  menu_layer_set_click_config_onto_window(keyboard, neutralize_window);
-
-  menu_layer_set_click_config_onto_window(keyboard_two, capture_window);
+  menu_layer_set_click_config_onto_window(player_menu, window);
+  menu_layer_set_click_config_onto_window(option_menu, window2);
+  menu_layer_set_click_config_onto_window(neutralize_keyboard, neutralize_window);
+  menu_layer_set_click_config_onto_window(capture_keyboard, capture_window);
 
 
 
   // Create a text layer and set the text
-  s_text_layer = text_layer_create(GRect(0, 0, 144, 30));
-  text_layer_set_text(s_text_layer, "Pick A Player!"); 
+  neutralize_code_text = text_layer_create(GRect(0, 0, 144, 30));
+  text_layer_set_text(neutralize_code_text, "Pick A Player!"); 
   // Set the font and text alignment
-  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  text_layer_set_font(neutralize_code_text, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_alignment(neutralize_code_text, GTextAlignmentCenter);
 
 
   // Create a text layer and set the text
-  s_text_layer_two = text_layer_create(GRect(0, 0, 144, 30));
-  text_layer_set_text(s_text_layer_two, "Choose An Option");
+  capture_code_text = text_layer_create(GRect(0, 0, 144, 30));
+  text_layer_set_text(capture_code_text, "Choose An Option");
   // Set the font and text alignment
-  text_layer_set_font(s_text_layer_two, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
-  text_layer_set_text_alignment(s_text_layer_two, GTextAlignmentCenter);
+  text_layer_set_font(capture_code_text, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_alignment(capture_code_text, GTextAlignmentCenter);
 
 
   // Create a text layer and set the text
@@ -386,6 +426,7 @@ void window_load (Window *window){
   text_layer_set_text_alignment(capture_input, GTextAlignmentCenter);
 
   //These are all of the callbacks
+  //Cal backs for each of the menu layers
   MenuLayerCallbacks callbacks = {
     .draw_row = (MenuLayerDrawRowCallback) draw_row_callback,
     .get_num_rows = (MenuLayerGetNumberOfRowsInSectionsCallback) num_rows_callback,
@@ -411,45 +452,48 @@ void window_load (Window *window){
   };
 
   // Set all the callbacks for the menu layer
-  menu_layer_set_callbacks(menu_layer, NULL, callbacks);
-  menu_layer_set_callbacks(menu_layer_two, NULL, callbackstwo);
-  menu_layer_set_callbacks(keyboard, NULL, callbacksthree);
-  menu_layer_set_callbacks(keyboard_two, NULL, callbacksfour);
+  menu_layer_set_callbacks(player_menu, NULL, callbacks);
+  menu_layer_set_callbacks(option_menu, NULL, callbackstwo);
+  menu_layer_set_callbacks(neutralize_keyboard, NULL, callbacksthree);
+  menu_layer_set_callbacks(capture_keyboard, NULL, callbacksfour);
 
 
   // Add it to the window for display
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_text_layer));
-  layer_add_child(window_get_root_layer(window), menu_layer_get_layer(menu_layer));
-  layer_add_child(window_get_root_layer(window2), text_layer_get_layer(s_text_layer_two));
-  layer_add_child(window_get_root_layer(window2), menu_layer_get_layer(menu_layer_two));
-  layer_add_child(window_get_root_layer(neutralize_window), menu_layer_get_layer(keyboard));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(neutralize_code_text));
+  layer_add_child(window_get_root_layer(window), menu_layer_get_layer(player_menu));
+  layer_add_child(window_get_root_layer(window2), text_layer_get_layer(capture_code_text));
+  layer_add_child(window_get_root_layer(window2), menu_layer_get_layer(option_menu));
+  layer_add_child(window_get_root_layer(neutralize_window), menu_layer_get_layer(neutralize_keyboard));
   layer_add_child(window_get_root_layer(neutralize_window), text_layer_get_layer(navigation_header));
   layer_add_child(window_get_root_layer(neutralize_window), text_layer_get_layer(navigation_input));
-  layer_add_child(window_get_root_layer(capture_window), menu_layer_get_layer(keyboard_two));
+  layer_add_child(window_get_root_layer(capture_window), menu_layer_get_layer(capture_keyboard));
   layer_add_child(window_get_root_layer(capture_window), text_layer_get_layer(capture_header));
   layer_add_child(window_get_root_layer(capture_window), text_layer_get_layer(capture_input));
 }
 
 //Unloads the window
 void window_unload (Window *window) {
-  //Destroy the layers
-  text_layer_destroy(s_text_layer);
-  text_layer_destroy(s_text_layer_two);
+
+  //Destroy the text layers
+  text_layer_destroy(neutralize_code_text);
+  text_layer_destroy(capture_code_text);
   text_layer_destroy(navigation_header);
   text_layer_destroy(navigation_input);
   text_layer_destroy(capture_input);
   text_layer_destroy(capture_header);
 
-  menu_layer_destroy(menu_layer);
-  menu_layer_destroy(menu_layer_two);
-  menu_layer_destroy(keyboard);
-  menu_layer_destroy(keyboard_two);
+  //Destroy the menu layers
+  menu_layer_destroy(player_menu);
+  menu_layer_destroy(option_menu);
+  menu_layer_destroy(neutralize_keyboard);
+  menu_layer_destroy(capture_keyboard);
 
 
 }
 
-
+//Initialize Method
 void init() {
+  //Creates all of the windows
   window = window_create();
   window2= window_create();
   neutralize_window= window_create();
@@ -467,18 +511,20 @@ void init() {
   window_stack_push (window, true);
 }
 
-
+//Deinitialize Method
 void deinit() {
+  //Destroys all of the windows
   window_destroy(window);
   window_destroy(window2);
   window_destroy(neutralize_window);
   window_destroy(capture_window);
 
-
 }
 
+//Main Method Runs the application 
 int main(void) {
   init();
   app_event_loop();
   deinit();
 }
+
